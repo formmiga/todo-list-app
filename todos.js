12931d052ac28b150affe6620f196f0b5f1e8607
Patricia Formiga <patriciaformigacontreiras@gmail.com>
2023-06-14 11:58:26 +0100

    implemented list
    Made an array for the todos list, adding some values in strings. Listed the todos with a For Loop, displaying the index to make it easier for the user whenever choosing which task to remove, in case they input 'delete'.
    
    In the while loop, created a if in case it's equal to 'list', with two console messages of dashes to separate the starter of the list in the console.
    
    Using the For loop, starting from index 0, adding one each time to the total length. The message in the console is written as a string template literal, adding the 'i' as a reference to the index and then the todos with the index (i).

--- todos.js
+++ todos.js
@@ -1,5 +1,14 @@
 let input = prompt("what would you like to do?");
+const todos = ["Clean litter box", "Buy cat food", "Adopt a new kitty!"];
+
 while (input !== "quit" && input !== "q") {
+  if (input === "list") {
+    console.log("------------");
+    for (let i = 0; i < todos.length; i++) {
+      console.log(`${i}: ${todos[i]}`);
+    }
+    console.log("------------");
+  }
   input = prompt("what would you like to do?");
 }
 console.log("ok, you quit the app!");
